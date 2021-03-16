ece7331390e64fc2c43d1d9645ee7656915cf26c
HOMECREDIT\AANosov <aanosov@homecredit.ru>
2021-03-16 18:09:37 +0300

    Lesson 1 task 1

--- GB_language_basics_Nosov/Program.cs
+++ GB_language_basics_Nosov/Program.cs
@@ -6,7 +6,17 @@ namespace GB_language_basics_Nosov
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Write("Введите имя: ");
+            var name = Console.ReadLine();
+            Console.Write("Введите фамилию: ");
+            var last_name = Console.ReadLine();
+            Console.Write("Введите возраст: ");
+            var age = Console.ReadLine();
+            Console.Write("Введите рост: ");
+            var growth = Console.ReadLine();
+            Console.Write("Введите вес: ");
+            var mass = Console.ReadLine();
+            Console.WriteLine(name + " " + last_name + "\nВозраст: " + age + "\nРост: " + "\nРост: " + age + "\nВес: " + mass);
         }
     }
 }
